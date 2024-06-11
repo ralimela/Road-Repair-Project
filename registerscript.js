1eee372abf564e3f6a37d2f47d3a06deba3a744e
NalajamHemanth <103932769+NalajamHemanth@users.noreply.github.com>
2024-06-11 16:50:27 +0530

    Update registerscript.js

--- registerscript.js
+++ registerscript.js
@@ -1,31 +1,50 @@
 function showLogin() {
-    window.location.href ='login.html'
- }
- function showRegister() {
-     window.location.href ='register.html';
- }
- document.getElementById("registrationForm").addEventListener("submit", function(event) {
-     event.preventDefault();
-     if (validateForm()) {
-         generateOTP();
-     } else {
-         alert("Please fill in all the details."); 
-     }
- });
- function validateForm() {
-     var firstname = document.getElementById("firstname").value;
-     var lastname = document.getElementById("lastname").value;
-     var mobileNumber = document.getElementById("mobileNumber").value;
-     if (firstname === "" || lastname === "" || mobileNumber === "") {
-         return false;
-     }
-     return true;
- }
- function generateOTP() {
+    window.location.href = 'login.html';
+}
+
+function showRegister() {
+    window.location.href = 'register.html';
+}
+
+document.getElementById("registrationForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+    if (validateForm()) {
+        generateOTP();
+    } else {
+        alert("Please fill in all the details."); 
+    }
+});
+
+function validateForm() {
+    var username = document.getElementById("username").value;
+    var email = document.getElementById("email").value;
+    var firstname = document.getElementById("firstname").value;
+    var lastname = document.getElementById("lastname").value;
+    var mobileNumber = document.getElementById("mobileNumber").value;
+    if (username === "" || email === "" || firstname === "" || lastname === "" || mobileNumber === "") {
+        return false;
+    }
+    return true;
+}
+
+function generateOTP() {
     var otp = Math.floor(100000 + Math.random() * 900000).toString();
-    console.log("Generated OTP:", otp); 
+    console.log("Generated OTP:", otp);
     alert("Your OTP is: " + otp);
+
+    var username = document.getElementById("username").value;
+    var email = document.getElementById("email").value;
+    var firstname = document.getElementById("firstname").value;
+    var lastname = document.getElementById("lastname").value;
+    var mobileNumber = document.getElementById("mobileNumber").value;
+
     localStorage.setItem("generatedOTP", otp);
-    console.log("OTP stored in localStorage");
+    localStorage.setItem("username", username);
+    localStorage.setItem("email", email);
+    localStorage.setItem("firstname", firstname);
+    localStorage.setItem("lastname", lastname);
+    localStorage.setItem("mobileNumber", mobileNumber);
+
+    console.log("User details stored in localStorage");
     window.location.href = 'otpregister.html';
 }
