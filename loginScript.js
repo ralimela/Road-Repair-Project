ebf0da942cc6a38746266be33803e26b39bb29d4
NalajamHemanth <103932769+NalajamHemanth@users.noreply.github.com>
2024-06-11 16:52:37 +0530

    Update and rename login1Script.js to loginScript.js

--- loginScript.js
+++ loginScript.js
@@ -1,9 +1,8 @@
 document.getElementById("loginForm").addEventListener("submit", function(event) {
     event.preventDefault();
-    console.log("Form submission prevented.");
     var mobileNumber = document.getElementById("mobileNumber").value;
     if (mobileNumber) {
-        console.log("Mobile number entered:", mobileNumber);
+        localStorage.setItem("mobileNumber", mobileNumber); // Store the mobile number
         generateOTP();
     } else {
         alert("Please enter your mobile number.");
@@ -12,10 +11,8 @@ document.getElementById("loginForm").addEventListener("submit", function(event) 
 
 function generateOTP() {
     var otp = Math.floor(100000 + Math.random() * 900000).toString();
-    console.log("Generated OTP:", otp); 
     alert("Your OTP is: " + otp);
     localStorage.setItem("generatedOTP", otp);
-    console.log("OTP stored in localStorage");
     window.location.href = 'otplogin.html';
 }
 
